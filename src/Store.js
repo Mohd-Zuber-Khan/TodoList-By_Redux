4edc0178e3a4366f236da0011dfde64811a40366
zuber <kzuberkhan0786@gmail.com>
2023-05-31 11:05:22 +0530

    TodoList

---
+++ src/Store.js
@@ -0,0 +1,8 @@
+ import { configureStore } from "@reduxjs/toolkit";
+import todoListReducer from "./Slices/TodoListSlice"
+
+ export default configureStore({
+    reducer:{
+        todo:todoListReducer 
+    }
+})   
